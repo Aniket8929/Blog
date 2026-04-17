f13c93e75c0f645646019c99851fed941104e6d8
Aniket8929 <aniketkumarrai271@gmail.com>
2026-04-17 12:11:53 +0530

    url updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const commentRoutes = require('./routes/comments');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://blog-five-rho-30.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
